56c00556726a845e43099f51c21fb43462d84794
Manas R <122677792+1deringmanas1331@users.noreply.github.com>
2023-10-10 20:03:05 +0530

    Add files via upload

---
+++ Day Predictor.py
@@ -0,0 +1,17 @@
+import datetime
+
+def predict_day_of_week():
+    while True:
+        try:
+            year = int(input("Enter the year (e.g., 2023): "))
+            month = int(input("Enter the month (1-12): "))
+            day = int(input("Enter the day (1-31): "))
+            input_date = datetime.date(year, month, day)
+            day_of_week = input_date.strftime("%A")
+            print(f"The day of the week for {input_date} is {day_of_week}.")
+            break
+        except ValueError:
+            print("Invalid date input. Please enter a valid date.")
+
+if __name__ == "__main__":
+    predict_day_of_week()
