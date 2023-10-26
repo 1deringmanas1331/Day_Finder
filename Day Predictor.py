9b5359112404629d7eea11b6c939e0ce434973db
Manas R <122677792+1deringmanas1331@users.noreply.github.com>
2023-10-26 07:14:12 +0530

    Day Predictor.py

--- Day Predictor.py
+++ Day Predictor.py
@@ -3,10 +3,13 @@ import datetime
 def predict_day_of_week():
     while True:
         try:
+            #Ask the user for the date
             year = int(input("Enter the year (e.g., 2023): "))
             month = int(input("Enter the month (1-12): "))
             day = int(input("Enter the day (1-31): "))
             input_date = datetime.date(year, month, day)
+            
+            #Find the day for the given date
             day_of_week = input_date.strftime("%A")
             print(f"The day of the week for {input_date} is {day_of_week}.")
             break
